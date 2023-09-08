fd1aa082f82f1b61511d59781fdaf8d3002fa872
5CF87E9ED4E06CE9EB14E658B5EF8F6F <144470827+5CF87E9ED4E06CE9EB14E658B5EF8F6F@users.noreply.github.com>
2023-09-08 21:52:13 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+# leap year
+
+num = int(input("Enter the year"))
+if num % 4 == 0:
+  print("The year is leap year", num)
+else:
+  print("The year is not leap year", num)
---
+++ main.py
@@ -0,0 +1,14 @@
+ #implement recursive function to calculate the factorial of a given number
+
+
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_rec(n - 1)
+
+
+number = int(input("Enter a value: "))
+res = fact_rec(number)
+
+print("The factorial of {} is {}".format(number, res))
